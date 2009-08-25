d422c12c44c8f69ba1fdad0119fa2a15ad61922a
Dan Weaver <dweaver@denver3048.dna.root.ad>
2009-08-25 16:15:11 -0600

    prettier code formatting

--- lib/calculate_business_days.rb
+++ lib/calculate_business_days.rb
@@ -6,15 +6,15 @@ module CalculateBusinessDays
   module ClassMethods
     def business_days_from_now(num, start_date=Date.today)
       #Turn the start date into a number, i.e. Monday = 1
-       start_day_of_week = start_date.cwday
+      start_day_of_week = start_date.cwday
 
-       ans, i = 0, 0
+      ans, i = 0, 0
 
-       weeks = num / 5.0
+      weeks = num / 5.0
 
-       temp_num = num > 5 ? 5 : num
+      temp_num = num > 5 ? 5 : num
 
-       begin
+      begin
         ans += days_to_adjust(start_day_of_week,temp_num, i)
         i += 1
         weeks -= 1.0
@@ -26,24 +26,24 @@ module CalculateBusinessDays
     end
 
     def days_to_adjust(start_day_of_week, num, i)
-       ansr = 0
-       case start_day_of_week
-       when 1
-         if 5 == num then ansr += 2 end
-       when 2
-         if (4..5).include?(num) then ansr += 2 end
-       when 3
-         if (3..5).include?(num) then ansr += 2 end
-       when 4
-         if (2..5).include?(num) then ansr += 2 end
-       when 5
-         ansr += 2
-       when 6
-         i > 0 ? ansr += 2 : ansr += 1 
-       when 7
-         if i >= 1 then ansr += 2 end
-       end
-       return ansr
+      ansr = 0
+      case start_day_of_week
+      when 1
+       if 5 == num then ansr += 2 end
+      when 2
+       if (4..5).include?(num) then ansr += 2 end
+      when 3
+       if (3..5).include?(num) then ansr += 2 end
+      when 4
+       if (2..5).include?(num) then ansr += 2 end
+      when 5
+       ansr += 2
+      when 6
+       i > 0 ? ansr += 2 : ansr += 1 
+      when 7
+       if i >= 1 then ansr += 2 end
+      end
+      return ansr
     end
   end
 end
